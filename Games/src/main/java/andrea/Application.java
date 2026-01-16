36fc98f7ff55c496eee03773ba148018830722c9
pask517 <andreapascarella99@gmail.com>
2026-01-16 09:43:11 +0100

    added base

---
+++ Games/src/main/java/andrea/Application.java
@@ -0,0 +1,8 @@
+package andrea;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Games/src/main/java/andrea/Game.java
@@ -0,0 +1,13 @@
+package andrea;
+
+public abstract class Game {
+    private long contatoreId = 0;
+    private long id;
+    private String title;
+    private int publicationYear;
+    private int price;
+
+    public Game() {
+
+    }
+}
